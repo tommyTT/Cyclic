4035434d6f088811a08678c853c09033faf1a3b4
sam <samson.bassett@gmail.com>
2017-05-21 20:20:30 -0700

    got facing updown working in fan, BUT breaks old builds/blocks. does not sync up with redstony. backup branch

--- src/main/java/com/lothrazar/cyclicmagic/component/fan/BlockFan.java
+++ src/main/java/com/lothrazar/cyclicmagic/component/fan/BlockFan.java
@@ -1,10 +1,16 @@
 package com.lothrazar.cyclicmagic.component.fan;
 import com.lothrazar.cyclicmagic.IHasRecipe;
+import com.lothrazar.cyclicmagic.ModCyclic;
 import com.lothrazar.cyclicmagic.block.BlockBaseFacingInventory;
 import com.lothrazar.cyclicmagic.gui.ModGuiHandler;
+import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -18,6 +24,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class BlockFan extends BlockBaseFacingInventory implements IHasRecipe {
   //block rotation in json http://www.minecraftforge.net/forum/index.php?topic=32753.0
+  public static final PropertyDirection FACINGALL = BlockDirectional.FACING;
   public BlockFan() {
     super(Material.ROCK, ModGuiHandler.GUI_INDEX_FAN);
     this.setHardness(3F);
@@ -25,11 +32,21 @@ public class BlockFan extends BlockBaseFacingInventory implements IHasRecipe {
     this.setSoundType(SoundType.WOOD);
     this.setTickRandomly(true);
     this.setTranslucent();
+    this.setDefaultState(this.blockState.getBaseState().withProperty(FACINGALL, EnumFacing.NORTH));
+    
   }
   @Override
   public TileEntity createTileEntity(World worldIn, IBlockState state) {
     return new TileEntityFan();
   }
+  @Override//overrride the BLockBaseFacing one
+  public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+  {
+    EnumFacing f = EnumFacing.getDirectionFromEntityLiving(pos, placer);
+    ModCyclic.logger.info("fan "+f);
+      return this.getDefaultState().withProperty(FACINGALL, f);
+  }
+
   @Override
   public boolean isSideSolid(IBlockState base_state, IBlockAccess world, BlockPos pos, EnumFacing side) {
     return side == EnumFacing.DOWN;
@@ -43,5 +60,20 @@ public class BlockFan extends BlockBaseFacingInventory implements IHasRecipe {
         'i', Items.IRON_INGOT,
         'r', Items.REPEATER,
         's', Blocks.STONE);
+  }
+  @Override
+  protected BlockStateContainer createBlockState()
+  {
+      return new BlockStateContainer(this, new IProperty[] {FACINGALL});
+  }
+  @Override
+  public int getMetaFromState(IBlockState state) {
+    int i = 0;//same as dispenser
+    return i | ((EnumFacing)state.getValue(FACINGALL)).getIndex();
+  }
+  @Override
+  public IBlockState getStateFromMeta(int meta)//same as dispenser
+  {
+      return this.getDefaultState().withProperty(FACINGALL, EnumFacing.getFront(meta & 7));//.withProperty(TRIGGERED, Boolean.valueOf((meta & 8) > 0));
   }
 }
